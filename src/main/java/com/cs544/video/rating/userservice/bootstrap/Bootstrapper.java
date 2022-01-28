65500941000ba4a1d22ad6b49e95d76743feac21
Tamirat Fisseha <tamiratfissehasitotaw@gmail.com>
2022-01-28 10:25:18 -0600

    first commit

---
+++ src/main/java/com/cs544/video/rating/userservice/bootstrap/Bootstrapper.java
@@ -0,0 +1,14 @@
+package com.cs544.video.rating.userservice.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bootstrapper implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
