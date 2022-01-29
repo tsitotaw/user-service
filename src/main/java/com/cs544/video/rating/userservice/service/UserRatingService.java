dd6651df37b9f6b2116f0950a096f6b62755accf
Tamirat Fisseha <tamiratfissehasitotaw@gmail.com>
2022-01-29 17:56:41 -0600

    use api gateway as an authentication server and change here to pass auth details

--- src/main/java/com/cs544/video/rating/userservice/service/UserRatingService.java
+++ src/main/java/com/cs544/video/rating/userservice/service/UserRatingService.java
@@ -6,10 +6,16 @@ import com.cs544.video.rating.userservice.model.User;
 import com.cs544.video.rating.userservice.model.UserRating;
 import com.cs544.video.rating.userservice.repository.IUserRatingRepository;
 import com.cs544.video.rating.userservice.repository.IUserRepository;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.HttpEntity;
+import org.apache.http.entity.BasicHttpEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+import org.springframework.http.HttpHeaders;
 import java.util.List;
 
 @Service
@@ -42,7 +48,21 @@ public class UserRatingService implements IUserRatingService{
         UserRating rating = this.userRatingRepository.findById(userRatingId).get();
         User user = this.userRepository.findById(rating.getUserId()).get();
 
-        Video video = this.restTemplate.getForObject("http://VR-API-GATEWAY/videos/"+rating.getVideoId(), Video.class);
+
+        String plainCreds = "user:user";
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+        String base64Creds = new String(base64CredsBytes);
+
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Basic " + base64Creds);
+        org.springframework.http.HttpEntity<Video> request = new org.springframework.http.HttpEntity<Video>(headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        ResponseEntity<Video> response = restTemplate.exchange("http://localhost:9111/videos/"+rating.getVideoId(), HttpMethod.GET, request, Video.class);
+        Video video = response.getBody();
 
         UserRatingVideo urv = new UserRatingVideo();
         urv.setUserRating(rating);
